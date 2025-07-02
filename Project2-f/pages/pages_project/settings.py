a2cf1eb6c892918515978b5fe48f5ed57a15cb9b
Super Hacker Sh0me <asanaly_serekbay@sjtu.edu.cn>
2025-07-02 17:23:48 +0800

    Heroku configuration

--- Project2-f/pages/pages_project/settings.py
+++ Project2-f/pages/pages_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-%1p(le^b2($%17(^&&gv7s96jayg=(##8zlg+k&b6#s6da1^9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
